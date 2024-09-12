33655fa219efab052b3c7578f5d8cd63297c2b71
sanghee0820 <lsh277604@gmail.com>
2024-09-12 23:15:10 +0900

    project initialization

---
+++ src/main/java/Inplace/Team7_BE/Team7BeApplication.java
@@ -0,0 +1,13 @@
+package Inplace.Team7_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Inplace/Team7_BE/Team7BeApplicationTests.java
@@ -0,0 +1,13 @@
+package Inplace.Team7_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
