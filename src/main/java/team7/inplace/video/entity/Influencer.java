b14a0ed7a86793d23bf24b5cfcc4980b5e8689b4
BaeJunho <bjh4498@knu.ac.kr>
2024-09-19 19:54:07 +0900

    feat : 도메인 초기 설정
    비즈니스 로직을 추가하지 않은 단순 도메인 클래스 생성 및 더미 데이터 삽입

---
+++ src/main/java/team7/inplace/video/entity/Influencer.java
@@ -0,0 +1,17 @@
+package team7.inplace.video.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Influencer {
+    /*
+     * 더미 데이터 입니다 !!!
+     */
+    @Id
+    private Long id;
+
+    protected Influencer() {
+
+    }
+}
