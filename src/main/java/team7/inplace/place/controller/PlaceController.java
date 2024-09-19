7e01efcd52413a55193e948adb836b2b82d0c249
dong_yxxn <g0326kdy@gmail.com>
2024-09-19 20:51:33 +0900

    [feat] 카테고리 리스트 반환 기능 구현
    Category 리스트를 반환받을 수 있도록 컨트롤러 구현했습니다.
    
    관련 이슈: #7

--- src/main/java/team7/inplace/place/controller/PlaceController.java
+++ src/main/java/team7/inplace/place/controller/PlaceController.java
@@ -1,2 +1,30 @@
-package team7.inplace.place.controller;public class PlaceController {
+package team7.inplace.place.controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team7.inplace.place.domain.Category;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+
+    @GetMapping("/categories")
+    public ResponseEntity<Map<String, List<String>>> getCategories() {
+        List<String> categories = Arrays.stream(Category.values())
+            .map(Enum::name)
+            .collect(Collectors.toList());
+
+        Map<String, List<String>> response = new HashMap<>();
+        response.put("categories", categories);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
