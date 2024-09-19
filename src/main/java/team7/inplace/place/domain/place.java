91a0d11271233c66315bacae46ed11442c98196d
dong_yxxn <g0326kdy@gmail.com>
2024-09-19 20:03:06 +0900

    [feat] place 엔티티 구현 place 엔티티를 구현했습니다. category를 enum타입으로 추가할 예정입니다.
    관련이슈: #7

---
+++ src/main/java/team7/inplace/place/domain/place.java
@@ -0,0 +1,64 @@
+package team7.inplace.place.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+public class place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, columnDefinition = "BIGINT(20)")
+    private Long placeId;
+
+    @Column(nullable = false, columnDefinition = "varchar(50)")
+    private String name;
+
+    @ColumnDefault("false")
+    @Column(nullable = false)
+    private boolean pet;
+
+    @ColumnDefault("false")
+    @Column(nullable = false)
+    private boolean wifi;
+
+    @ColumnDefault("false")
+    @Column(nullable = false)
+    private boolean parking;
+
+    @ColumnDefault("false")
+    @Column(nullable = false)
+    private boolean fordisabled;
+
+    @ColumnDefault("false")
+    @Column(nullable = false)
+    private boolean nursery;
+
+    @ColumnDefault("false")
+    @Column(nullable = false)
+    private boolean smokingroom;
+
+    @Column(nullable = false, columnDefinition = "varchar(50)")
+    private String address1;
+
+    @Column(nullable = false, columnDefinition = "varchar(50)")
+    private String address2;
+
+    @Column(nullable = false, columnDefinition = "varchar(50)")
+    private String address3;
+
+    @Column
+    private String menuImgUrl;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String longitude;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String latitude;
+}
