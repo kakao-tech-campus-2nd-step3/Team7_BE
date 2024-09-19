8177c74b7a76543fc05839a8cc646d7d05413226
dong_yxxn <g0326kdy@gmail.com>
2024-09-19 21:20:29 +0900

    [refactor] 카테고리 목록 반환 로직 서비스단으로 이동
    Category 목록 반환 로직을 Service 단에서 반환받도록 수정하였습니다.
    
    관련 이슈: #7

--- src/main/java/team7/inplace/place/controller/PlaceController.java
+++ src/main/java/team7/inplace/place/controller/PlaceController.java
@@ -5,22 +5,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team7.inplace.place.domain.Category;
+import team7.inplace.place.service.PlaceService;
 
 @RestController
 @RequestMapping("/places")
 public class PlaceController {
+    private final PlaceService placeService;
 
+    @Autowired
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
     @GetMapping("/categories")
     public ResponseEntity<Map<String, List<String>>> getCategories() {
-        List<String> categories = Arrays.stream(Category.values())
-            .map(Enum::name)
-            .collect(Collectors.toList());
+        List<String> categories = placeService.getCategories();
 
         Map<String, List<String>> response = new HashMap<>();
         response.put("categories", categories);
