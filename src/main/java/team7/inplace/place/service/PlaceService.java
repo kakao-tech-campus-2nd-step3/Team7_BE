adaedd128862f67a7ad34bd54396581a7dbb051a
dong_yxxn <g0326kdy@gmail.com>
2024-09-19 21:17:37 +0900

    [feat] PlaceService 인터페이스 구현

---
+++ src/main/java/team7/inplace/place/service/PlaceService.java
@@ -0,0 +1,8 @@
+package team7.inplace.place.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+public interface PlaceService {
+    List<String> getCategories();
+}
---
+++ src/main/java/team7/inplace/place/service/PlaceServiceImpl.java
@@ -0,0 +1,2 @@
+package team7.inplace.place.service;public class PlaceServiceImpl {
+}
