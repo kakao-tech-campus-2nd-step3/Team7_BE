62ef1fe226f4634ad3b0881315bbb82d7a70a45c
dong_yxxn <g0326kdy@gmail.com>
2024-09-19 21:19:07 +0900

    [feat] 카테로리 목록 반환 기능 구현
    카테고리 enum의 값들을 String으로 변환 후, 리스트로 반환하였습니다.
    
    관련 이슈: #7

--- src/main/java/team7/inplace/place/service/PlaceServiceImpl.java
+++ src/main/java/team7/inplace/place/service/PlaceServiceImpl.java
@@ -1,2 +1,18 @@
-package team7.inplace.place.service;public class PlaceServiceImpl {
+package team7.inplace.place.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import team7.inplace.place.domain.Category;
+
+@Service
+public class PlaceServiceImpl implements PlaceService{
+
+    @Override
+    public List<String> getCategories() {
+        return Arrays.stream(Category.values())
+            .map(Enum::name)
+            .collect(Collectors.toList());
+    }
 }
