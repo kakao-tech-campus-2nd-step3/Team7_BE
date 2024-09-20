2c198de1eb152de81a0a7c0a048e0bceb751ccb5
BaeJunho <bjh4498@knu.ac.kr>
2024-09-20 14:28:56 +0900

    feat : #4 형식에 맞게 비디오 정보를 반환하는 기능 구현
    비디오 별로 새로운 Alias를 지어주는 부분 외, 정보를 조회하고 API 문서에 맞는 형식으로 반환하는 기능을 구현

--- src/main/java/team7/inplace/video/entity/Place.java
+++ src/main/java/team7/inplace/video/entity/Place.java
@@ -1,15 +1,20 @@
 package team7.inplace.video.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Place {
     /*
      * 더미 데이터 입니다 !!!
      */
     @Id
     private Long id;
+    @Column
+    private String name;
 
     protected Place(){
 
