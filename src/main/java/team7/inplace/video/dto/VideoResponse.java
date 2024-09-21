5e97e5e6ac88724febd523e4e51ca2609378de87
BaeJunho <bjh4498@knu.ac.kr>
2024-09-21 15:59:51 +0900

    chore : 주석 추가 ( 클래스 설명 )

--- src/main/java/team7/inplace/video/dto/VideoResponse.java
+++ src/main/java/team7/inplace/video/dto/VideoResponse.java
@@ -2,6 +2,7 @@ package team7.inplace.video.dto;
 
 import team7.inplace.place.dto.PlaceForVideo;
 
+// Video 엔티티의 Controller Response 정보 전달을 담당하는 클래스
 public record VideoResponse(Long videoId, String videoAlias, String videoUrl, PlaceForVideo place) {
     public VideoResponse(VideoData videoData) {
         this(videoData.videoId(), videoData.videoAlias(), videoData.videoUrl(), videoData.place());
