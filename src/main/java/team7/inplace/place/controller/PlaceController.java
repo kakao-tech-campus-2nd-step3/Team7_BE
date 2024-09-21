e852d9192121a062cb4ce4feb3ed934829cbd32e
dong_yxxn <g0326kdy@gmail.com>
2024-09-21 20:48:17 +0900

    [refactor]: 코드리뷰 반영
    - PlaceService 인터페이스 제거, getCategories()에서 List 반환
    - Place id의 nullable=false 제거
    - CategoryListDTO -> CategoryListResponse로 이름 변경해서 직관화함
    
    관련 이슈: #7

--- src/main/java/team7/inplace/place/controller/PlaceController.java
+++ src/main/java/team7/inplace/place/controller/PlaceController.java
@@ -1,26 +1,27 @@
 package team7.inplace.place.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team7.inplace.place.domain.CategoryListDTO;
+import team7.inplace.place.domain.Category;
+import team7.inplace.place.domain.CategoryListResponse;
 import team7.inplace.place.service.PlaceService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/places")
 public class PlaceController {
+
     private final PlaceService placeService;
 
-    @Autowired
-    public PlaceController(PlaceService placeService) {
-        this.placeService = placeService;
-    }
     @GetMapping("/categories")
-    public ResponseEntity<CategoryListDTO> getCategories() {
-        CategoryListDTO response = placeService.getCategories();
+    public ResponseEntity<CategoryListResponse> getCategories() {
+        List<Category> categories = placeService.getCategories();
+        CategoryListResponse response = new CategoryListResponse(categories);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/team7/inplace/place/domain/CategoryListResponse.java
+++ src/main/java/team7/inplace/place/domain/CategoryListResponse.java
@@ -2,6 +2,6 @@ package team7.inplace.place.domain;
 
 import java.util.List;
 
-public record CategoryListDTO(List<Category> categories) {
+public record CategoryListResponse(List<Category> categories) {
 
 }
