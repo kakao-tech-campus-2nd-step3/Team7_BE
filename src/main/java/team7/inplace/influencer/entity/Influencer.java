21c15f6ba9b4d746fb4c8db9ece8af2f73e93664
BaeJunho <bjh4498@knu.ac.kr>
2024-09-22 19:01:54 +0900

    refactor : 테스트를 위한 생성자 추가 및 필드 변수 설정
    id를 제외한 필드를 사용하여 생성자를 만들고, 이를 통해 테스트를 진행하기 위하여 @NonNull과 @RequiredArgsConstructor을 사용하여 리펙토링

--- src/main/java/team7/inplace/influencer/entity/Influencer.java
+++ src/main/java/team7/inplace/influencer/entity/Influencer.java
@@ -1,25 +1,26 @@
 package team7.inplace.influencer.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access= AccessLevel.PROTECTED)
+@RequiredArgsConstructor // 테스팅을 위한 부분 추가, 협의 하에 다른 방식 채택 가능
 public class Influencer {
     /*
      * 더미 데이터 입니다 !!!
      */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
+    @NonNull
     private String name;
     @Column
+    @NonNull
     private String job;
     @Column
+    @NonNull
     private String imgUrl;
 }
--- src/main/java/team7/inplace/video/entity/Video.java
+++ src/main/java/team7/inplace/video/entity/Video.java
@@ -3,22 +3,28 @@ package team7.inplace.video.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import team7.inplace.influencer.entity.Influencer;
 import team7.inplace.place.domain.Place;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@RequiredArgsConstructor // 테스팅을 위한 부분 추가, 협의 하에 다른 방식 채택 가능
 public class Video {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name="video_url", nullable = false, columnDefinition = "TEXT")
+    @NonNull
     private String videoUrl;
     @ManyToOne
     @JoinColumn(name = "influencer_id", nullable = false)
+    @NonNull
     private Influencer influencer;
     @ManyToOne
     @JoinColumn(name = "place_id", nullable = false)
+    @NonNull
     private Place place;
 }
