e8e8b90febd838217a90cf59cfbf191aded60719
suhyeon7497 <nucc1186@gmail.com>
2024-09-25 18:43:30 +0900

    [fix] security 설정 없으면 오류 발생 해결
    security에 대한 설정이 없으면 애플리케이션 실행 안돼서
    application.yml 파일 추가
    
    관련 이슈: #2

--- src/main/java/team7/inplace/video/entity/Video.java
+++ src/main/java/team7/inplace/video/entity/Video.java
@@ -1,17 +1,25 @@
 package team7.inplace.video.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team7.inplace.place.domain.Place;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Video {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="video_url")
+    @Column(name = "video_url")
     private String videoUrl;
     @ManyToOne
     @JoinColumn(name = "influencer_id")
