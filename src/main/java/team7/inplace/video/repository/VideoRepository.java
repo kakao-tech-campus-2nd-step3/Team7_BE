21d3c6eabe490b066a0721168238730af97a059c
hyunseo <wndlthsk@naver.com>
2024-09-26 17:20:39 +0900

    [chore] 충돌 파일 임시로 삭제 및 주석 처리

--- src/main/java/team7/inplace/video/repository/VideoRepository.java
+++ src/main/java/team7/inplace/video/repository/VideoRepository.java
@@ -3,8 +3,6 @@ package team7.inplace.video.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team7.inplace.video.entity.Video;
 
-import java.util.List;
-
 public interface VideoRepository extends JpaRepository<Video, Long> {
-    List<Video> findVideosByInfluencerIdIn(List<Long> influencerIds);
+//    List<Video> findVideosByInfluencerIdIn(List<Long> influencerIds);
 }
