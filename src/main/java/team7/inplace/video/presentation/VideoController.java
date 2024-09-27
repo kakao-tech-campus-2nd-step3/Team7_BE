19ae5d44b3bbfca687e3723c5f3973d57b2287ab
sanghee0820 <lsh277604@gmail.com>
2024-09-27 16:15:05 +0900

    [refactor] import문 최적화

--- src/main/java/team7/inplace/video/presentation/VideoController.java
+++ src/main/java/team7/inplace/video/presentation/VideoController.java
@@ -1,16 +1,15 @@
 package team7.inplace.video.presentation;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import team7.inplace.video.application.VideoService;
 import team7.inplace.video.application.dto.VideoInfo;
 import team7.inplace.video.presentation.dto.VideoResponse;
-import team7.inplace.video.application.VideoService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +22,7 @@ public class VideoController {
             @RequestParam(name = "influencer", required = false) List<String> influencers
     ) {
         List<VideoInfo> videoInfos = videoService.findByInfluencer(influencers);
-        List<VideoResponse> videoResponses = videoInfos.stream().map(VideoResponse::new).toList();
+        List<VideoResponse> videoResponses = videoInfos.stream().map(VideoResponse::from).toList();
         return new ResponseEntity<>(videoResponses, HttpStatus.OK);
     }
 }
