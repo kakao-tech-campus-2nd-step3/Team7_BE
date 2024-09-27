975d6118e5af10a9576e8307d0e3eb2269f36714
BaeJunho <bjh4498@knu.ac.kr>
2024-09-27 13:46:22 +0900

    [feat] Swagger 사용하여 Controller 메서드 설명 작성
    성공 및 실패 사례는 아직 미처리, 추후 추가 예정

--- src/main/java/team7/inplace/video/presentation/VideoController.java
+++ src/main/java/team7/inplace/video/presentation/VideoController.java
@@ -1,5 +1,6 @@
 package team7.inplace.video.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,9 @@ import java.util.List;
 public class VideoController {
     private final VideoService videoService;
 
-    // 내 인플루언서가 방문한 그 곳 ( 토큰 0 )
+    // 토큰 필요 메서드
     @GetMapping("/video")
+    @Operation(summary = "내 인플루언서가 방문한 그곳", description = "인플루언서를 기준으로 Video 정보를 조회합니다.")
     public ResponseEntity<List<VideoResponse>> readByInfluencer(
             @RequestParam(name = "influencer", required = false) List<String> influencers
     ) {
@@ -28,9 +30,10 @@ public class VideoController {
         return new ResponseEntity<>(videoResponses, HttpStatus.OK);
     }
 
-    // 내 주변 그 곳 ( 토큰 x )
-    // Place 쪽에 기능을 넣어야 할 것 같다 ( 내 위치 기준으로 가까운 장소 가져오기 )
+    // 토큰 불필요 메서드
+    // 내 주변 장소 반환 기능 ( PR 존재 ) weekly에 merge 시 개발
     @GetMapping("/video")
+    @Operation(summary = "내 주변 그 곳", description = "내 위치를 기준으로 Video 정보를 조회합니다.")
     public ResponseEntity<List<VideoResponse>> readBySurround(
             @RequestParam(name = "longitude") String longtitude,
             @RequestParam(name = "latitude") String latitude
@@ -39,17 +42,17 @@ public class VideoController {
         return new ResponseEntity<>(videoResponses, HttpStatus.OK);
     }
 
-    // 새로 등록된 그 곳
     @GetMapping("/videos/new")
+    @Operation(summary = "새로 등록된 그 곳", description = "id를 기준으로 내림차순 정렬한 Video 정보를 조회합니다.")
     public ResponseEntity<List<VideoResponse>> readByNew() {
         List<VideoInfo> videoInfos = videoService.findAllDESC();
         List<VideoResponse> videoResponses = videoInfos.stream().map(VideoResponse::of).toList();
         return new ResponseEntity<>(videoResponses, HttpStatus.OK);
     }
 
-    // 쿨한 그 곳
-    // 조회수 가져오는 로직 개발 전에 완성 못함
+    // 조회수 반환 기능 개발 시 개발
     @GetMapping("/videos/cool")
+    @Operation(summary = "쿨한 그 곳", description = "조회수를 기준으로 내림차순 정렬한 Video 정보를 조회합니다.")
     public ResponseEntity<List<VideoResponse>> readByCool() {
         List<VideoResponse> videoResponses = new ArrayList<>();
         return new ResponseEntity<>(videoResponses, HttpStatus.OK);
