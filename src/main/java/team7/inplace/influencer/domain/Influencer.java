bee184a72b75d9d81df78e0be4b02ebe7f91c2a6
sanghee0820 <lsh277604@gmail.com>
2024-09-27 17:56:15 +0900

    [refactor] 패키지명 변경

--- src/main/java/team7/inplace/influencer/domain/Influencer.java
+++ src/main/java/team7/inplace/influencer/domain/Influencer.java
@@ -1,7 +1,15 @@
-package team7.inplace.influencer.entity;
+package team7.inplace.influencer.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
