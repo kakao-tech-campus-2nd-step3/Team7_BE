46f3834b7865b9426dc7f31a27ea30a575d15081
BaeJunho <bjh4498@knu.ac.kr>
2024-09-29 00:30:21 +0900

    [chore] 사용하지 않는 import 문 삭제

--- src/main/java/team7/inplace/video/persistence/VideoRepository.java
+++ src/main/java/team7/inplace/video/persistence/VideoRepository.java
@@ -1,6 +1,5 @@
 package team7.inplace.video.persistence;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team7.inplace.place.domain.Place;
 import team7.inplace.video.domain.Video;
--- src/main/java/team7/inplace/video/presentation/VideoController.java
+++ src/main/java/team7/inplace/video/presentation/VideoController.java
@@ -2,7 +2,6 @@ package team7.inplace.video.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
