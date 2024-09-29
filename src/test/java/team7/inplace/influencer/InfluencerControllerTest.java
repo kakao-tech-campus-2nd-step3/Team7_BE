7759fc9542eea27e47f2267d25e9c1ca5b6d6f1a
hyunseo <wndlthsk@naver.com>
2024-09-29 13:37:50 +0900

    [test] 컨트롤러 테스트 수정

--- src/test/java/team7/inplace/influencer/InfluencerControllerTest.java
+++ src/test/java/team7/inplace/influencer/InfluencerControllerTest.java
@@ -1,33 +1,45 @@
 package team7.inplace.influencer;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import team7.inplace.influencer.application.InfluencerService;
 import team7.inplace.influencer.application.dto.InfluencerDto;
 import team7.inplace.influencer.presentation.InfluencerController;
+import team7.inplace.influencer.presentation.dto.InfluencerListResponse;
 
 @ExtendWith(MockitoExtension.class)
-@WebMvcTest(InfluencerController.class)
 public class InfluencerControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private InfluencerService influencerService;
+
+    @InjectMocks
+    private InfluencerController influencerController;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(influencerController).build();
+    }
 
     @Test
     public void getAllInfluencersTest() throws Exception {
@@ -35,21 +47,24 @@ public class InfluencerControllerTest {
             false);
         InfluencerDto influencerDto2 = new InfluencerDto(2L, "influencer2", "imgUrl2", "job2",
             false);
-        List<InfluencerDto> influencersDtoList = Arrays.asList(influencerDto1, influencerDto2);
-        given(influencerService.getAllInfluencers()).willReturn(influencersDtoList);
+        List<InfluencerDto> influencerDtoList = Arrays.asList(influencerDto1, influencerDto2);
+
+        InfluencerListResponse response1 = InfluencerListResponse.convertToResponse(influencerDto1);
+        InfluencerListResponse response2 = InfluencerListResponse.convertToResponse(influencerDto2);
+        List<InfluencerListResponse> responseList = List.of(response1, response2);
+
+        given(influencerService.getAllInfluencers()).willReturn(influencerDtoList);
+
+        // response를 json으로 변경
+        ObjectMapper objectMapper = new ObjectMapper();
+        String expectedJson = objectMapper.writeValueAsString(responseList);
 
         mockMvc.perform(get("/influencers")
                 .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$[0].influencerId").value(1L))
-            .andExpect(jsonPath("$[0].influencerName").value("influencer1"))
-            .andExpect(jsonPath("$[0].influencerImgUrl").value("imgUrl1"))
-            .andExpect(jsonPath("$[0].influencerJob").value("job1"))
-            .andExpect(jsonPath("$[0].likes").value(false))
-            .andExpect(jsonPath("$[1].influencerId").value(2L))
-            .andExpect(jsonPath("$[1].influencerName").value("influencer2"))
-            .andExpect(jsonPath("$[1].influencerImgUrl").value("imgUrl2"))
-            .andExpect(jsonPath("$[1].influencerJob").value("job2"))
-            .andExpect(jsonPath("$[1].likes").value(false));
+            .andExpect(content().json(expectedJson));
+
+        verify(influencerService, times(1)).getAllInfluencers(); // 서비스 호출 확인
     }
 }
