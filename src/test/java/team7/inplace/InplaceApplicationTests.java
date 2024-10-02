82360f5fcb34c0480cdcacd4039f1d4a59f7ebc5
sanghee0820 <lsh277604@gmail.com>
2024-10-02 20:52:45 +0900

    [refactor] 테스트 코드 패키지 변경

--- src/test/java/team7/inplace/InplaceApplicationTests.java
+++ src/test/java/team7/inplace/InplaceApplicationTests.java
@@ -1,4 +1,4 @@
-package team7.inplace.be;
+package team7.inplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InplaceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
