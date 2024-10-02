554ac45cd6334e2bdbfbb6a6bba90cc08ecf4441
sanghee0820 <lsh277604@gmail.com>
2024-10-02 23:38:52 +0900

    [feat] 채널 최근 기록 업데이트 로직 추가 및 주석 추가

--- src/main/java/team7/inplace/crawling/application/YoutubeCrawlingService.java
+++ src/main/java/team7/inplace/crawling/application/YoutubeCrawlingService.java
@@ -1,8 +1,11 @@
 package team7.inplace.crawling.application;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import team7.inplace.crawling.application.dto.RawVideoInfo;
 import team7.inplace.crawling.client.YoutubeClient;
 import team7.inplace.crawling.repository.YoutubeChannelRepository;
 
@@ -15,14 +18,18 @@ public class YoutubeCrawlingService {
 
     /*
         1. 유튜브 채널 정보를 모두 가져온다.
-        2.
+        2. 마지막 비디오와, 유튜브 UUID를 이용하여 비디오 정보를 가져온다.
+        3. 마지막 비디오 UUID를 업데이트 한다.
+        4. 카카오 API를 호출해 장소 정보를 가져온다
      */
     public void crawlAllVideos() {
         var youtubeChannels = youtubeChannelRepository.findAll();
-
-        var videos = youtubeChannels.stream()
-                .map(channel -> youtubeClient.getVideos(channel.getPlayListUUID(), channel.getLastVideoUUID()))
-                .toList();
+        List<RawVideoInfo> rawVideoInfos = new ArrayList<>();
+        for (var channel : youtubeChannels) {
+            var videos = youtubeClient.getVideos(channel.getPlayListUUID(), channel.getLastVideoUUID());
+            rawVideoInfos.addAll(videos);
+            channel.updateLastVideoUUID(videos.get(0).videoId());
+        }
     }
 }
 
