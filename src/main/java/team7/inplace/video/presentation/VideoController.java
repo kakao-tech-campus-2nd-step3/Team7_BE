42e19233457de5a120707b1819eddc9947cada16
BaeJunho <bjh4498@knu.ac.kr>
2024-10-03 20:14:46 +0900

    [refactor] API 명세 인터페이스에 작성하고 구현하기
    Controller를 구현체로 Interface를 만들어 명세를 작성하는 스타일 채택

--- src/main/java/team7/inplace/video/presentation/VideoController.java
+++ src/main/java/team7/inplace/video/presentation/VideoController.java
@@ -1,6 +1,5 @@
 package team7.inplace.video.presentation;
 
-import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -19,15 +18,11 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/videos")
-public class VideoController {
+public class VideoController implements VideoControllerApiSpec{
     private final VideoService videoService;
 
     // 토큰 필요 메서드
     @GetMapping()
-    @Operation(
-            summary = "내 인플루언서가 방문한 or 내 주변 그곳 ",
-            description = "토큰의 유무에 따라 다른 동작을 수행합니다."
-    )
     public ResponseEntity<List<VideoResponse>> readVideos(
             HttpServletRequest request,
             @RequestParam(name = "influencer", required = false) List<String> influencers,
@@ -63,7 +58,6 @@ public class VideoController {
     }
 
     @GetMapping("/new")
-    @Operation(summary = "새로 등록된 그 곳", description = "id를 기준으로 내림차순 정렬한 Video 정보를 조회합니다.")
     public ResponseEntity<List<VideoResponse>> readByNew() {
         List<VideoInfo> videoInfos = videoService.findAllDesc();
         List<VideoResponse> videoResponses = videoInfos.stream().map(VideoResponse::from).toList();
@@ -72,7 +66,6 @@ public class VideoController {
 
     // 조회수 반환 기능 개발 시 개발
     @GetMapping("/cool")
-    @Operation(summary = "쿨한 그 곳", description = "조회수를 기준으로 내림차순 정렬한 Video 정보를 조회합니다.")
     public ResponseEntity<List<VideoResponse>> readByCool() {
         List<VideoResponse> videoResponses = new ArrayList<>();
         return new ResponseEntity<>(videoResponses, HttpStatus.OK);
