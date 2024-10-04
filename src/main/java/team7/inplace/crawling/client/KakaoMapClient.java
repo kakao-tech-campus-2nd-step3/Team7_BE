be39b8b0a177df13a740fbb16c0e6a6daaa936fd
sanghee0820 <lsh277604@gmail.com>
2024-10-04 16:41:14 +0900

    [feat] place 정보 반환 추가 및 카테고리 정보 추가

--- src/main/java/team7/inplace/crawling/client/KakaoMapClient.java
+++ src/main/java/team7/inplace/crawling/client/KakaoMapClient.java
@@ -1,7 +1,7 @@
 package team7.inplace.crawling.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
@@ -10,11 +10,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import team7.inplace.crawling.client.dto.RawPlace;
 import team7.inplace.crawling.client.dto.RawVideoInfo;
 import team7.inplace.global.kakao.config.KakaoApiProperties;
 
-@Component
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class KakaoMapClient {
     private static final String KAKAO_MAP_LOCATE_SEARCH_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
@@ -23,18 +24,22 @@ public class KakaoMapClient {
     private final KakaoApiProperties kakaoApiProperties;
     private final RestTemplate restTemplate;
 
-    public void search(List<RawVideoInfo> videoInfos) {
-        for (RawVideoInfo videoInfo : videoInfos) {
-            var address = videoInfo.address();
-            var locationInfo = getLocateInfo(address);
-            var placeId = locationInfo.get("documents").get(0).get("id").asText();
-            var placeInfo = getPlaceInfo(placeId);
-            log.info("KakaoMapClient placeInfo : " + placeInfo);
+    public RawPlace.Info search(RawVideoInfo videoInfo, String category) {
+        var address = videoInfo.address();
+        var locationInfo = getLocateInfo(address, category);
+        var placeId = locationInfo.has("documents") ?
+                locationInfo.get("documents").get(0).get("id").asText() : null;
+        if (Objects.isNull(placeId)) {
+            return null;
         }
+
+        var placeInfo = getPlaceInfo(placeId);
+        return RawPlace.Info.from(locationInfo, placeInfo);
     }
 
-    private JsonNode getLocateInfo(String address) {
+    private JsonNode getLocateInfo(String address, String category) {
         var url = KAKAO_MAP_LOCATE_SEARCH_URL + KAKAO_MAP_LOCATE_SEARCH_PARAMS.formatted(address);
+        url = url + (Objects.isNull(category) ? "" : "&category_group_code=" + category);
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", kakaoApiProperties.getAuthorization());
