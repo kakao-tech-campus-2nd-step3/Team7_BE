ec7114bd063cd5ad26ced508853bb861b84acb5d
BaeJunho <bjh4498@knu.ac.kr>
2024-10-04 14:06:16 +0900

    [chore] 사용하지 않는 import 문 제거

--- src/main/java/team7/inplace/video/domain/Video.java
+++ src/main/java/team7/inplace/video/domain/Video.java
@@ -3,7 +3,6 @@ package team7.inplace.video.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
