419891985995bd822275721d7b2bb4bb162e142c
wndlthsk <80496766+wndlthsk@users.noreply.github.com>
2024-10-04 14:36:09 +0900

    [test] cicd 테스트용 컨트롤러

--- src/main/java/team7/inplace/cicd/TestController.java
+++ src/main/java/team7/inplace/cicd/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/cicd")
     public ResponseEntity test() {
-        return ResponseEntity.ok("ci/cd 테스트");
+        return ResponseEntity.ok("ci/cd 테스트2");
     }
 }
