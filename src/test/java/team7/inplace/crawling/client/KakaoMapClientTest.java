9784d54d379c7411c13fb12beaf822434e739b07
sanghee0820 <lsh277604@gmail.com>
2024-10-04 16:43:08 +0900

    [test]: 단일 비디오 정보 조회변경 및 카테고리 정보 추가

--- src/test/java/team7/inplace/crawling/client/KakaoMapClientTest.java
+++ src/test/java/team7/inplace/crawling/client/KakaoMapClientTest.java
@@ -1,6 +1,5 @@
 package team7.inplace.crawling.client;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +17,10 @@ class KakaoMapClientTest {
     @DisplayName("카카오 맵 주소 검색 테스트")
     void searchAddressTest() {
         // given
-        var address = "서울특별시 강남구 테헤란로 521";
+        var address = "대구 북구 대현남로6길 25";
         var rawVideoInfo = new RawVideoInfo("test", "test", address);
 
         // when
-        KakaoMapClient.search(List.of(rawVideoInfo));
+        KakaoMapClient.search(rawVideoInfo, "FD6");
     }
 }
