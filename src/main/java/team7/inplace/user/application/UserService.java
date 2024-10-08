d6b08e6e2928a7d5ed33dd8e2eb8b896218ac717
BaeJunho <bjh4498@knu.ac.kr>
2024-10-08 23:23:13 +0900

    [refactor] 패키지 이름 변경

--- src/main/java/team7/inplace/user/application/UserService.java
+++ src/main/java/team7/inplace/user/application/UserService.java
@@ -3,10 +3,9 @@ package team7.inplace.user.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team7.inplace.influencer.application.dto.InfluencerInfo;
 import team7.inplace.influencer.domain.Influencer;
-import team7.inplace.like.domain.Like;
-import team7.inplace.like.persistent.LikeRepository;
+import team7.inplace.favorite.domain.Favorite;
+import team7.inplace.favorite.persistent.FavoriteRepository;
 import team7.inplace.user.application.dto.UserCommand;
 import team7.inplace.user.domain.User;
 import team7.inplace.user.persistence.UserRepository;
@@ -18,7 +17,7 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final LikeRepository likeRepository;
+    private final FavoriteRepository likeRepository;
 
     @Transactional
     public void registerUser(UserCommand.Create userCreate) {
@@ -38,7 +37,7 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public List<Long> getInfluencerIdsByUsername(String username) {
-        List<Like> likes = likeRepository.findByUser(userRepository.findByUsername(username).orElseThrow());
-         return likes.stream().map(Like::getInfluencer).map(Influencer::getId).toList();
+        List<Favorite> likes = likeRepository.findByUser(userRepository.findByUsername(username).orElseThrow());
+         return likes.stream().map(Favorite::getInfluencer).map(Influencer::getId).toList();
     }
 }
