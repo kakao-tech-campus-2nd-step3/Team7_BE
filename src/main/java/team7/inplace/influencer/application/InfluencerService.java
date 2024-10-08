42d7ce3ba5443c352d39243a64841e27a0f87b47
hyunseo <wndlthsk@naver.com>
2024-10-08 17:45:03 +0900

    [feat] 인플루언서 추가,수정,삭제 기능

--- src/main/java/team7/inplace/influencer/application/InfluencerService.java
+++ src/main/java/team7/inplace/influencer/application/InfluencerService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team7.inplace.influencer.application.dto.InfluencerCommand;
 import team7.inplace.influencer.application.dto.InfluencerInfo;
+import team7.inplace.influencer.domain.Influencer;
 import team7.inplace.influencer.persistence.InfluencerRepository;
 
 @RequiredArgsConstructor
@@ -18,5 +20,27 @@ public class InfluencerService {
         return influencerRepository.findAll().stream()
             .map(InfluencerInfo::from)
             .toList();
+    }
+
+    @Transactional
+    public Long createInfluencer(InfluencerCommand command) {
+        Influencer influencer = InfluencerCommand.to(command);
+        return influencerRepository.save(influencer).getId();
+    }
+
+    @Transactional
+    public Long updateInfluencer(Long id, InfluencerCommand command) {
+        Influencer influencer = influencerRepository.findById(id).orElseThrow();
+        influencer.update(command.influencerName(), command.influencerImgUrl(),
+            command.influencerJob());
+
+        return influencer.getId();
+    }
+
+    @Transactional
+    public void deleteInfluencer(Long id) {
+        Influencer influencer = influencerRepository.findById(id).orElseThrow();
+
+        influencerRepository.delete(influencer);
     }
 }
--- src/main/java/team7/inplace/influencer/presentation/InfluencerController.java
+++ src/main/java/team7/inplace/influencer/presentation/InfluencerController.java
@@ -4,11 +4,18 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import team7.inplace.influencer.application.InfluencerService;
+import team7.inplace.influencer.application.dto.InfluencerCommand;
 import team7.inplace.influencer.application.dto.InfluencerInfo;
 import team7.inplace.influencer.presentation.dto.InfluencerListResponse;
+import team7.inplace.influencer.presentation.dto.InfluencerRequest;
 import team7.inplace.influencer.presentation.dto.InfluencerResponse;
 
 @RequiredArgsConstructor
@@ -28,4 +35,35 @@ public class InfluencerController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @PostMapping("/influencers")
+    public ResponseEntity<Long> createInfluencer(@RequestBody InfluencerRequest request) {
+        InfluencerCommand influencerCommand = new InfluencerCommand(
+            request.influencerName(),
+            request.influencerImgUrl(),
+            request.influencerJob()
+        );
+        Long savedId = influencerService.createInfluencer(influencerCommand);
+
+        return new ResponseEntity<>(savedId, HttpStatus.OK);
+    }
+
+    @PutMapping("/influencers/{id}")
+    public ResponseEntity<Long> updateInfluencer(@PathVariable Long id,
+        @RequestBody InfluencerRequest request) {
+        InfluencerCommand influencerCommand = new InfluencerCommand(
+            request.influencerName(),
+            request.influencerImgUrl(),
+            request.influencerJob()
+        );
+        Long updatedId = influencerService.updateInfluencer(id, influencerCommand);
+
+        return new ResponseEntity<>(updatedId, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/influencers/{id}")
+    public ResponseEntity<Long> deleteInfluencer(@PathVariable Long id) {
+        influencerService.deleteInfluencer(id);
+
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
 }
