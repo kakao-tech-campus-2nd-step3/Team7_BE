4e0629b435b862732f65f8f2ef0e90c35c53a84c
sanghee0820 <lsh277604@gmail.com>
2024-10-09 18:08:04 +0900

    [feat] command 생성 매서드 추가

--- src/main/java/team7/inplace/crawling/application/dto/CrawlingInfo.java
+++ src/main/java/team7/inplace/crawling/application/dto/CrawlingInfo.java
@@ -3,10 +3,23 @@ package team7.inplace.crawling.application.dto;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 import team7.inplace.crawling.client.dto.PlaceNode;
+import team7.inplace.place.application.command.PlacesCommand;
+import team7.inplace.video.application.command.VideoCommand;
 
 public record CrawlingInfo(
         Long influencerId,
         List<JsonNode> videoSnippets,
         List<PlaceNode> placeNodes
 ) {
+    public List<VideoCommand.Create> toVideoCommands() {
+        return videoSnippets.stream()
+                .map(snippet -> VideoCommand.Create.from(snippet, influencerId))
+                .toList();
+    }
+
+    public List<PlacesCommand.Create> toPlacesCommands() {
+        return placeNodes.stream()
+                .map(placeNode -> PlacesCommand.Create.from(placeNode.locationNode(), placeNode.placeNode()))
+                .toList();
+    }
 }
