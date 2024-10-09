212083b0f50523839a47bc6f98310e18c3a3c0fb
sanghee0820 <lsh277604@gmail.com>
2024-10-09 18:07:35 +0900

    [feat] video, place 생성 로직 추가

--- src/main/java/team7/inplace/place/domain/OffDay.java
+++ src/main/java/team7/inplace/place/domain/OffDay.java
@@ -12,4 +12,15 @@ public class OffDay {
     private String holydayName;
     private String weekAndDay;
     private String temporaryHolidays;
+
+    private OffDay(String holydayName, String weekAndDay, String temporaryHolidays) {
+        this.holydayName = holydayName;
+        this.weekAndDay = weekAndDay;
+        this.temporaryHolidays = temporaryHolidays;
+    }
+
+    public static OffDay of(String offDay) {
+        String[] offDayArray = offDay.split("\\|");
+        return new OffDay(offDayArray[0], offDayArray[1], offDayArray[2]);
+    }
 }
