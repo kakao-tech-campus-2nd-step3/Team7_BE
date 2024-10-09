22b3dccfb3909b7a774e9bb3d9f45431c486cb05
suhyeon7497 <nucc1186@gmail.com>
2024-10-09 17:15:08 +0900

    [refactor] 필요 없는 코드 삭제
    pr에서 받은 피드백을 반영하였습니다.
    
    관련 이슈 : #2

--- src/main/java/team7/inplace/security/application/CustomOAuth2UserService.java
+++ src/main/java/team7/inplace/security/application/CustomOAuth2UserService.java
@@ -1,6 +1,6 @@
 package team7.inplace.security.application;
 
-import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -12,20 +12,12 @@ import team7.inplace.user.application.UserService;
 import team7.inplace.user.application.dto.UserCommand;
 import team7.inplace.user.domain.Role;
 
+@RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final DefaultOAuth2UserService defaultOAuth2UserService;
     private final UserService userService;
 
-    public CustomOAuth2UserService(
-        DefaultOAuth2UserService defaultOAuth2UserService,
-        UserService userService
-    ) {
-        this.defaultOAuth2UserService = defaultOAuth2UserService;
-        this.userService = userService;
-    }
-
-    @Transactional
     @Override
     public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest)
         throws OAuth2AuthenticationException {
--- src/main/java/team7/inplace/security/config/SecurityFilterConfig.java
+++ src/main/java/team7/inplace/security/config/SecurityFilterConfig.java
@@ -2,19 +2,17 @@ package team7.inplace.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import team7.inplace.security.filter.AuthorizationFilter;
 import team7.inplace.security.filter.ExceptionHandlingFilter;
+import team7.inplace.security.util.JwtUtil;
 
-@Component
 @Configuration
 public class SecurityFilterConfig {
 
-    /* Bean으로 등록하여 SecurityConfig에서 주입 받아 사용할 경우 h2-console 사용 불가
     @Bean
     public AuthorizationFilter authorizationFilter(JwtUtil jwtUtil) {
         return new AuthorizationFilter(jwtUtil);
     }
-    */
 
     @Bean
     public ExceptionHandlingFilter exceptionHandlingFilter() {
