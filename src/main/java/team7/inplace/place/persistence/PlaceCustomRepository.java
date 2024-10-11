ffef64424a89f91f2c12b54da1564709475860ab
dong_yxxn <g0326kdy@gmail.com>
2024-10-11 20:55:11 +0900

    [fix] PlaceService에서 video null 처리 구현
    - placeService에서 video가 조회되지 않는 오류를 예외처리하였습니다.
    
    관련 이슈: #7

--- src/main/java/team7/inplace/place/persistence/PlaceCustomRepository.java
+++ src/main/java/team7/inplace/place/persistence/PlaceCustomRepository.java
@@ -1,27 +1,21 @@
 package team7.inplace.place.persistence;
 
-import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
-import team7.inplace.place.domain.Place;
-
 public interface PlaceCustomRepository {
 
-    Page<Place> findPlacesByDistance(
-        @Param("longitude") String longitude,
-        @Param("latitude") String latitude,
-        Pageable pageable);
-
-    Page<Place> findPlacesByDistanceAndFilters(
-        @Param("topLeftLongitude") String topLeftLongitude,
-        @Param("topLeftLatitude") String topLeftLatitude,
-        @Param("bottomRightLongitude") String bottomRightLongitude,
-        @Param("bottomRightLatitude") String bottomRightLatitude,
-        @Param("longitude") String longitude,
-        @Param("latitude") String latitude,
-        @Param("categories") List<String> categories,
-        @Param("influencers") List<String> influencers,
-        Pageable pageable);
+//    Page<Place> findPlacesByDistance(
+//        @Param("longitude") String longitude,
+//        @Param("latitude") String latitude,
+//        Pageable pageable);
+//
+//    Page<Place> findPlacesByDistanceAndFilters(
+//        @Param("topLeftLongitude") String topLeftLongitude,
+//        @Param("topLeftLatitude") String topLeftLatitude,
+//        @Param("bottomRightLongitude") String bottomRightLongitude,
+//        @Param("bottomRightLatitude") String bottomRightLatitude,
+//        @Param("longitude") String longitude,
+//        @Param("latitude") String latitude,
+//        @Param("categories") List<String> categories,
+//        @Param("influencers") List<String> influencers,
+//        Pageable pageable);
 
 }
