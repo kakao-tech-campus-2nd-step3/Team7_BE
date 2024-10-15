9e8b198ff79b03e8643b7b70db238c66897eb6e4
sanghee0820 <lsh277604@gmail.com>
2024-10-15 10:21:34 +0900

    [feat] 트랜젝션 내 유튜브 채널 최신 영상 업데이트 로직 추가

--- src/main/java/team7/inplace/video/application/VideoFacade.java
+++ src/main/java/team7/inplace/video/application/VideoFacade.java
@@ -2,11 +2,11 @@ package team7.inplace.video.application;
 
 import java.util.List;
 import java.util.Objects;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import team7.inplace.crawling.application.YoutubeCrawlingService;
 import team7.inplace.global.annotation.Facade;
 import team7.inplace.global.exception.InplaceException;
 import team7.inplace.global.exception.code.AuthorizationErrorCode;
@@ -22,16 +22,18 @@ import team7.inplace.video.application.dto.VideoInfo;
 public class VideoFacade {
     private final VideoService videoService;
     private final PlaceService placeService;
+    private final YoutubeCrawlingService youtubeCrawlingService;
     private final UserService userService;
 
     @Transactional
     public void createVideos(List<VideoCommand.Create> videoCommands, List<PlacesCommand.Create> placeCommands) {
 
         var placeIds = placeService.createPlaces(placeCommands);
+        youtubeCrawlingService.updateLastVideoUUID(videoCommands.get(0).influencerId(), videoCommands.get(0).videoId());
         videoService.createVideos(videoCommands, placeIds);
     }
 
-    public Page<VideoInfo> getVideosByMyInfluencer(Pageable pageable){
+    public Page<VideoInfo> getVideosByMyInfluencer(Pageable pageable) {
         // User 정보를 쿠키에서 추출
         Long userId = AuthorizationUtil.getUserId();
         // 토큰 정보에 대한 검증
